0b658fdef53e7ab23b9ba6d5cd31a54b8ca1b67f
jogopin <jonn.g.pineda@gmail.com>
2023-03-29 11:07:48 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+
+
+function App() {
+
+  return (
+    <div className="app">
+      APP
+    </div>
+  )
+}
+
+export default App
